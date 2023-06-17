35d5c88a3dc7412906d12777361343dd826e95a2
Crip Zone <9021853+Crip-Zone@users.noreply.github.com>
2023-06-17 15:56:15 -0400

    Changed wording

--- languages/Settings.english.php
+++ languages/Settings.english.php
@@ -5,6 +5,6 @@ global $settings;
 
 // argument(s): images_url as saved in settings
 $txt['theme_thumbnail_href'] = '%1$s/thumbnail.png';
-$txt['theme_description'] = 'The default theme from Simple Machines.<br><br>Author: The Simple Machines Team';
-
+$txt['theme_description'] = 'The CleanWhite theme from Crip Zone Themes.<br><br>Author: Crip<br><br>Updated to v2.1 by The CripZone:<br>
+							Special thanks to: Sych0, Live627, shawnb61 and Bigguy';
 ?>
