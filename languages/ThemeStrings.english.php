2d2192dbd3175294d4b99037329eaa694764edc4
Crip Zone <9021853+Crip-Zone@users.noreply.github.com>
2023-07-06 19:34:54 -0400

    Updated ThemeStringsenglish.php
    Added the copyright.

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,6 @@
+<?php
+$txt['Navigate'] = 'Navigate';
+$txt['Design'] = 'Design';
+$txt['copyrights'] = 'Copyrights';
+$txt['themecopyright'] = 'CleanTek by <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="Crip Zone">Crip</a> <br> Updated for SMF 2.1<br>By <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="The Crip Zone Team">The Crip Zone Team</a>';
+?>
