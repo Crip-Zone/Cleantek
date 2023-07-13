073d4e1e0831f9b01ce8b406565ad1ed65c7d35c
Crip Zone <9021853+Crip-Zone@users.noreply.github.com>
2023-07-12 20:54:29 -0400

    Update ThemeStrings.english.php
    Text change

--- languages/ThemeStrings.english.php
+++ languages/ThemeStrings.english.php
@@ -2,5 +2,5 @@
 $txt['Navigate'] = 'Navigate';
 $txt['Design'] = 'Design';
 $txt['copyrights'] = 'Copyrights';
-$txt['themecopyright'] = 'CleanTek by <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="Crip Zone">Crip</a> <br> Updated for SMF 2.1<br>By <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="The Crip Zone Team">The Crip Zone Team</a>';
+$txt['themecopyright'] = 'CleanTek by <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="Crip Zone">Crip</a> <br> Updated for SMF 2.1<br>By <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="TheCripZone">TheCripZone</a>';
 ?>
